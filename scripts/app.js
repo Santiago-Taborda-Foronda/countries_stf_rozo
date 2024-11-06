e4ab82a4aeaf27fd6f1f4cee6f941743470c66de
Sena <SENA@ARMSCCCCTGOD155.FormacionSena.RED>
2024-11-06 17:39:43 -0500

    countries

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
+import { countries } from "../scripts/countries.js"
+ 
+
+ 
 const makeCountry = (countriy) => {
     const container = document.createElement('div')
     container.classList.add('card-country')
+
+    container.addEventListener('mouseenter', showInfo)
 
     const img = document.createElement('img')
     img.src = countriy.flag
@@ -13,4 +19,26 @@ const makeCountry = (countriy) => {
     container.appendChild(title)
 
     document.querySelector('main > section').appendChild(container)
+
+   
 }
+
+const renderCountries = () =>{
+countries.forEach(country => makeCountry(country))
+}
+
+
+// children convierte el div en un arreglo 
+const showInfo = (event) => {
+    let currentCountry = (event.target.children[1].textContent)
+    let foundCountry = countries.find(country => country.name === currentCountry)
+
+    const country = document.querySelector('#demographics')
+    country.children[0].textContent = foundCountry.name
+    country.children[1].textContent = foundCountry.capital
+    country.children[2].textContent = foundCountry.population
+    // const infoCountry = document.getElementById('demographics')
+    // console.log(foundCountry)
+}
+
+window.addEventListener('DOMContentLoaded', renderCountries())
